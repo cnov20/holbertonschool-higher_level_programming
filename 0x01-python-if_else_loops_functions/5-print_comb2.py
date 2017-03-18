8eea29149f75841cb4020034e0dd53ae46394387
cnov20 <128@holbertonschool.com>
2017-03-18 23:29:05 +0000

    Removed third, empty print() function call

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 99):
+for i in range(0, 100):
     if i < 10:
-        print('0{:d}, '.format(i), end="")
+        print('0{:d}, '.format(i), end='')
         continue
-    print('{:d}, '.format(i), end="")
-print(99)
+    print(', {:d}'.format(i), end='')

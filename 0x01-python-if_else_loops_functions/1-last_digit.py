0451854414c6016fc8019608ef998b69fcff0097
cnov20 <128@holbertonschool.com>
2017-03-18 19:50:32 +0000

    Task 1 - conditional print of last digit - need to fix functionality to show negative last digits - Initial

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+
+if number < 0 or last_digit < 0:
+    print("Last digit of " + '{:d}'.format(number) + " is "
+          + '{:d}'.format(-last_digit) + " and is less than 6 and not zero")
+
+if last_digit > 5:
+    print("Last digit of " + '{:d}'.format(number) + " is "
+          + '{:d}'.format(last_digit) + " and is greater than 5")
+elif last_digit == 0:
+    print("Last digit of " + '{:d}'.format(number) + " is "
+          + '{:d}'.format(last_digit) + " and is 0")
+elif last_digit < 6:
+    print("Last digit of " + '{:d}'.format(number) + " is "
+          + '{:d}'.format(last_digit) + " and is less than 6 and not zero")

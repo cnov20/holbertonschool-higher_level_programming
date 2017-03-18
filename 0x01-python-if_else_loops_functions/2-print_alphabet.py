1eac17460a5bdc25049b8454e2ca9e7986edba41
cnov20 <128@holbertonschool.com>
2017-03-18 22:15:22 +0000

    Added beginning range in 'for' loop, adjusted print to remove new line with each iteration, added print() call outside of loop to add new line after alphabet is prtinted

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-character = 97
-for character in range(123):
-    print(chr(character))
+character = 97  # 'a' ascii value
+for character in range(97, 123):  # ending 'z' ascii value - 123
+    print(chr(character), end="")
+
+print()

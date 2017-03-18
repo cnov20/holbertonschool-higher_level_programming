6d6665fe562b4397d1c994bdbcd334fb488083cf
cnov20 <128@holbertonschool.com>
2017-03-18 21:56:05 +0000

    Changed order of conditional statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,17 +2,17 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
+last_digit_negative = -last_digit
 
-if number < 0 or last_digit < 0:
+if number < 0 and last_digit != 0:
     print("Last digit of " + '{:d}'.format(number) + " is "
-          + '{:d}'.format(-last_digit) + " and is less than 6 and not zero")
-
-if last_digit > 5:
-    print("Last digit of " + '{:d}'.format(number) + " is "
-          + '{:d}'.format(last_digit) + " and is greater than 5")
-elif last_digit == 0:
-    print("Last digit of " + '{:d}'.format(number) + " is "
-          + '{:d}'.format(last_digit) + " and is 0")
+          + '{:d}'.format(last_digit_negative) + " and is less than 6 and not zero")
 elif last_digit < 6:
     print("Last digit of " + '{:d}'.format(number) + " is "
           + '{:d}'.format(last_digit) + " and is less than 6 and not zero")
+elif last_digit == 0:
+    print("Last digit of " + '{:d}'.format(number) + " is "
+          + '{:d}'.format(last_digit) + " and is 0")
+elif last_digit > 5:
+    print("Last digit of " + '{:d}'.format(number) + " is "
+          + '{:d}'.format(last_digit) + " and is greater than 5")

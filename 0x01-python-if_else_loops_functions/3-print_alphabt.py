4e688964eed1f7c7dfb443c7ad31d167b991b30d
cnov20 <128@holbertonschool.com>
2017-03-18 22:43:10 +0000

    Added continue statement, changed conditional statement

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 for char in range(97, 123):
-    if char != 101 and char != 114:
-        print('{:s}'.format(chr(char)), end="")
+    if char == 101 or char == 113:
+        continue
+    print('{:s}'.format(chr(char)), end="")

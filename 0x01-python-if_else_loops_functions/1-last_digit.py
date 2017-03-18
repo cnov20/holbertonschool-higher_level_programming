6903b3117547e77d3f46979da926775a7a35ff3b
cnov20 <128@holbertonschool.com>
2017-03-18 22:07:32 +0000

    Changed order of conditionals again, start with if last_digit == 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,15 +4,16 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 last_digit_negative = -last_digit
 
-if number < 0 and last_digit != 0:
-    print("Last digit of " + '{:d}'.format(number) + " is "
-          + '{:d}'.format(last_digit_negative) + " and is less than 6 and not zero")
+if last_digit == 0:
+    print("Last digit of " + '{:d}'.format(number) + " is " +
+          '{:d}'.format(last_digit) + " and is 0")
+elif number < 0:
+    print("Last digit of " + '{:d}'.format(number) + " is " +
+          '{:d}'.format(last_digit_negative) +
+          " and is less than 6 and not zero")
 elif last_digit < 6:
-    print("Last digit of " + '{:d}'.format(number) + " is "
-          + '{:d}'.format(last_digit) + " and is less than 6 and not zero")
-elif last_digit == 0:
-    print("Last digit of " + '{:d}'.format(number) + " is "
-          + '{:d}'.format(last_digit) + " and is 0")
+    print("Last digit of " + '{:d}'.format(number) + " is " +
+          '{:d}'.format(last_digit) + " and is less than 6 and not zero")
 elif last_digit > 5:
-    print("Last digit of " + '{:d}'.format(number) + " is "
-          + '{:d}'.format(last_digit) + " and is greater than 5")
+    print("Last digit of " + '{:d}'.format(number) + " is " +
+          '{:d}'.format(last_digit) + " and is greater than 5")

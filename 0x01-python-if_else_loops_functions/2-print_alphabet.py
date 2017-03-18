efb754fe4d1f9e5972bd8c3fdb338fbeb1ccef29
cnov20 <128@holbertonschool.com>
2017-03-18 22:21:15 +0000

    Refactored code to account for task specifications - removed storage in variable

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-character = 97  # 'a' ascii value
-for character in range(97, 123):  # ending 'z' ascii value - 123
-    print(chr(character), end="")
+
+for i in range(97, 123):
+    print(chr(i), end="")
+    i += 1

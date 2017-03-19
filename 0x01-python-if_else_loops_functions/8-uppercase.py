6e0f016249061bdcad34115906bba559b3eedbc0
cnov20 <128@holbertonschool.com>
2017-03-19 01:38:01 +0000

    Changed string formmatter

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             upper = chr(ord(c) - 32)
-        print('{:c}'.format(upper), end='')
+        print('{:s}'.format(upper), end='')
     print()

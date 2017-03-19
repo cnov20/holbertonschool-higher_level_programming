9cf135a9f7ed1ebb1c0deed648c7663e85d657ca
cnov20 <128@holbertonschool.com>
2017-03-19 20:53:07 +0000

    changed variable name from length to argc

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-    length = len(argv)
-    if length == 1:
-        print ('{:d} argument.'.format(length - 1))
-    elif length == 2:
-        print('{:d} argument:'.format(length - 1))
+    from sys import argv
+    argc = len(argv)
+    if argc == 1:
+        print ('{:d} argument.'.format(argc - 1))
+    elif argc == 2:
+        print('{:d} argument:'.format(argc - 1))
     else:
-        print('{:d} arguments:'.format(length - 1))
-    for i in range(1, length):
+        print('{:d} arguments:'.format(argc - 1))
+    for i in range(1, argc):
         print('{:d}: {:s}'.format(i, argv[i]))

9f4cf8fc57ab108ea05f39e6650f5c96613d86d6
cnov20 <128@holbertonschool.com>
2017-03-19 03:47:02 +0000

    Fixed typo in output statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,10 +10,10 @@ if last_digit == 0:
 elif number < 0:
     print("Last digit of " + '{:d}'.format(number) + " is " +
           '{:d}'.format(last_digit_negative) +
-          " and is less than 6 and not zero")
+          " and is less than 6 and not 0")
 elif last_digit < 6 and last_digit != 0:
     print("Last digit of " + '{:d}'.format(number) + " is " +
-          '{:d}'.format(last_digit) + " and is less than 6 and not zero")
+          '{:d}'.format(last_digit) + " and is less than 6 and not 0")
 elif last_digit > 5:
     print("Last digit of " + '{:d}'.format(number) + " is " +
           '{:d}'.format(last_digit) + " and is greater than 5")

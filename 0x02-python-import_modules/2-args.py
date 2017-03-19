903784c15da30da430ff97db2c0fb575816067fc
cnov20 <128@holbertonschool.com>
2017-03-19 15:21:09 +0000

    Added for loop and adjusted conditional statements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    i = 0
+    i = 1
     arg = "argument"
     args = "arguments"
     length = len(argv)
 
     if length == 1:
         print ('{:d} {:s}.'.format(length - 1, arg))
+
     elif length == 2:
         print('{:d} {:s}:'.format(length - 1, arg))
         print('{:d}: {:s}'.format(length - 1, argv[1]))
-    else:
-        for i in range(1, length, i+1):
-            print('{:d} {:s}:'.format(length - 1, args))
+
+    elif length >= 3:
+          print('{:d} {:s}:'.format(length - 1, args))
+
+          for i in range(i, length, +1):
             print('{:d}: {}'.format(length - 1, argv[i]))

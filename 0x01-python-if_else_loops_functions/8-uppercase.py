6abe26e7126eb08c944ae5e11cf11e9d27b7ec1d
cnov20 <128@holbertonschool.com>
2017-03-19 01:24:27 +0000

    Removed chr() function call - test

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,5 @@
 def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            print('{:c}'.format(chr(ord(c) - 32)))
+            print('{:c}'.format(ord(c) - 32))
     print()

8be18e8a91fa9b525e0605dbfeb1e6d8f81137fd
cnov20 <128@holbertonschool.com>
2017-03-19 01:21:09 +0000

    Add print() function calls and chr() function call

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return chr(i - 32)
-    else:
-        return False
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            print('{:c}'.format(chr(ord(c) - 32)))
+    print()

6f1c6eb3a414735a4e0f8d3727801760c4ac9f33
cnov20 <128@holbertonschool.com>
2017-03-19 17:38:07 +0000

    Deleted string variables and refactored conditional statements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    arg = 'argument'
-    args = 'arguments'
     length = len(argv)
 
     if length == 1:
-        print ('{:d} {:s}.'.format(length - 1, arg))
-
+        print ('{:d} argument.'.format(length - 1))
     elif length == 2:
-        print('{:d} {:s}:'.format(length - 1, arg))
-        print('{:d}: {:s}'.format(length - 1, argv[1]))
-
-    elif length >= 3:
-        print('{:d} {:s}:'.format(length - 1, args))
-        for i in range(1, length):
+        print('{:d} argument:'.format(length - 1))
+    else:
+        print('{:d} arguments:'.format(length - 1))
+    for i in range(1, length):
             print('{:d}: {:s}'.format(i, argv[i]))

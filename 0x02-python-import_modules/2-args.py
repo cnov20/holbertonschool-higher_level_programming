2683c9bddc1d48686844c46bb4ec7fd226be31af
cnov20 <128@holbertonschool.com>
2017-03-19 20:51:02 +0000

    Moved import module to top of file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+from sys import argv
 if __name__ == "__main__":
-    from sys import argv
     length = len(argv)
     if length == 1:
         print ('{:d} argument.'.format(length - 1))

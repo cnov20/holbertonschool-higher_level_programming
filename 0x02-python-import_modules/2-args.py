e58e4f9ddebee86f1810552e455f7ae2933c7e63
cnov20 <128@holbertonschool.com>
2017-03-19 20:59:06 +0000

    Changed to import sys rather than from import - argv specifically

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    argc = len(argv)
+    import sys
+    argc = len(sys.argv)
     if argc == 1:
         print ('{:d} argument.'.format(argc - 1))
     elif argc == 2:
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print('{:d} arguments:'.format(argc - 1))
     for i in range(1, argc):
-        print('{:d}: {:s}'.format(i, argv[i]))
+        print('{:d}: {:s}'.format(i, sys.argv[i]))

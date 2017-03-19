625e6e064dad0363b52d259d5d5f3f5943d5e2db
cnov20 <128@holbertonschool.com>
2017-03-19 21:02:51 +0000

    Changed import statement and adjusted spacing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+
+from sys import argv
+
 if __name__ == "__main__":
-    import sys
-    argc = len(sys.argv)
+
+    argc = len(argv)
+
     if argc == 1:
         print ('{:d} argument.'.format(argc - 1))
     elif argc == 2:
         print('{:d} argument:'.format(argc - 1))
     else:
         print('{:d} arguments:'.format(argc - 1))
+
     for i in range(1, argc):
-        print('{:d}: {:s}'.format(i, sys.argv[i]))
+        print('{:d}: {:s}'.format(i, argv[i]))

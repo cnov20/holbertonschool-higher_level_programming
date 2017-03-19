8b6265c6772d0770c3951ad06e3d97701d67f53e
cnov20 <128@holbertonschool.com>
2017-03-19 01:36:37 +0000

    Added variable assignment and separated expression from print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,6 @@
 def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            print('{:c}'.format(chr(ord(c) - 32)), end='')
+            upper = chr(ord(c) - 32)
+        print('{:c}'.format(upper), end='')
     print()

64bc67b426582735302b74ab02f0334fb107a634
cnov20 <128@holbertonschool.com>
2017-03-21 07:26:11 +0000

    Shortened variable names

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     sums = []
-    new_tuple_a = tuple_a + (0, 0)
-    new_tuple_b = tuple_b + (0, 0)
+    new_a = tuple_a + (0, 0)
+    new_b = tuple_b + (0, 0)
     for i in range(len(tuple_a)):
-        sums.append(new_tuple_a[i] + new_tuple_b[i])
+        sums.append(new_a[i] + new_b[i])
     return(sums)

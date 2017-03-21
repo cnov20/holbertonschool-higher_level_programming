3933999c0b44798ae6783edacc6e02606bec7165
cnov20 <128@holbertonschool.com>
2017-03-21 11:28:12 +0000

    Changed for loop to only account for twointeger values within tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,12 +2,9 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if tuple_a is None or tuple_b is None:
         return
-
-    #if len(tuple_a) < 2 or len(tuple_b) < 2:
-
     sums = []
     new_a = tuple_a + (0, 0)
     new_b = tuple_b + (0, 0)
-    for i in range(len(tuple_a)):
-        sums.append(new_a[i] + new_b[i])
+    for i in range(2):
+        sums.append(new_a[0] + new_b[0])
     return(tuple(sums))

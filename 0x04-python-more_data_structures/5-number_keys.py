2fa14ef7d45bd2eb0bd4b0b43967d01bd2f6168f
cnov20 <128@holbertonschool.com>
2017-03-21 19:01:58 +0000

    Added return(None) to handle None condition

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def number_keys(my_dict):
     num_keys = 0
-    if my_dict:
+    if my_dict is not None:
         for k, v in enumerate(range(len(my_dict))):
             num_keys += k
         return (num_keys)
+    return (None)

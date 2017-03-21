a522085d20aaf07a9c5bc031d22df1e4cb849a46
cnov20 <128@holbertonschool.com>
2017-03-21 15:59:51 +0000

    Added range and length

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(my_dict):
     num_keys = 0
-    for k, v in enumerate(my_dict):
+    for k, v in enumerate(range(len(my_dict))):
         num_keys += k
     return (num_keys)

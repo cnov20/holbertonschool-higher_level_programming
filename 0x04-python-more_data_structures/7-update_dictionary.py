09544e2b30269424b5a3bb44aa1fdbfefa9c549c
cnov20 <128@holbertonschool.com>
2017-03-21 19:08:28 +0000

    Removed if statement, since key/value replacement/creation happens, if key exists or not

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(my_dict, key, value):
-        if key and value not in my_dict:
-            my_dict[key] = value
-            return (my_dict)
+        my_dict[key] = value
+        return (my_dict)

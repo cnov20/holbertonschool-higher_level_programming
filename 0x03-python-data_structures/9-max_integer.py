76f550a7ae05b3d2a3231c657b37deec4124f3c1
cnov20 <128@holbertonschool.com>
2017-03-21 06:03:36 +0000

    Added functionality to handle negative values and changed variable name for max val

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@ def max_integer(my_list=[]):
     if not my_list:
         return
 
-    answer = 0
+    max_value = my_list[0]
     for i in my_list:
-        if i > answer:
-            answer = i
-    return(answer)
+        if i > max_value:
+            max_value = i
+    return(max_value)

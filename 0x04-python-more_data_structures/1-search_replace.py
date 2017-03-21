f12369f52cff048b10e28d47a97fea7db2024a87
cnov20 <128@holbertonschool.com>
2017-03-21 14:21:07 +0000

    Re-factored code - list comprehnesion

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-            return(new_list)
+    new_list = [replace if i == search else i for i in my_list]
+    return(new_list)

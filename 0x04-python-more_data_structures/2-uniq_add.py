fddfe0913022dced2747d585603a218b0dab293f
cnov20 <128@holbertonschool.com>
2017-03-21 14:49:47 +0000

    Converted list to set so only unique numbers are added

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    result = sum(map(lambda x: my_list[x], my_list[:]))
+    my_set = set(my_list)
+    result = sum(map(lambda x: x, my_set))
     return (result)

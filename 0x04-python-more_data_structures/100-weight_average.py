2cbceb4dd245094c9285199cdc54818bc14e28f0
cnov20 <128@holbertonschool.com>
2017-03-21 20:08:02 +0000

    Added for loop

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,4 +5,9 @@ def weight_average(my_list=[]):
     elif my_list is None:
         return (None)
     else:
-        return(my_list)
+        for score in range(len(my_list)):
+            weight = my_list[score][1]
+            weight_avg = (score * weight / weight)
+#            print (weight)
+ #           print (weight_avg)
+        return (weight_avg)

af2ed2fabe6622319f99f9e2f210da3a8fa9eb8d
cnov20 <128@holbertonschool.com>
2017-03-21 04:28:13 +0000

    Re-factored conditional statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ''
     for char in range(len(my_string)):
-        if 'C' in my_string or 'c' in my_string:
-            return(my_string[1:])
-        else:
-            return (my_string)
+        if my_string[char].lower() != 'c':
+            new_string += my_string[char]
+    return(new_string)

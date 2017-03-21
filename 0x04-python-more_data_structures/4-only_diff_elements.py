6ebec38dd7e267642f484971e175ba9fb5a05490
cnov20 <128@holbertonschool.com>
2017-03-21 15:32:31 +0000

    Task 4 - return only different elements between two sets - Initial

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    list_1 = list(set_1)
+    list_2 = list(set_2)
+    full_list = list_1 + list_2
+    return (common for common in full_list if common not in list_1 or common not in list_2)

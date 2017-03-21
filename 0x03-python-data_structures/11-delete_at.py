8c0eabedbab916ff3c2cf96aeea4518c4729fd4c
cnov20 <128@holbertonschool.com>
2017-03-21 06:51:27 +0000

    Removed for loop - added conditional for length of list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new_list = []
-    for i in range(len(my_list)):
+    if idx <= len(my_list):
         del my_list[idx]
-        return(new_list + my_list)
+    return(new_list + my_list)

19928ba2bf29970aa1ba21a36540eef98c11e3c7
cnov20 <128@holbertonschool.com>
2017-03-21 06:48:31 +0000

    Added for loop - length of list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-
     new_list = []
-    del my_list[idx]
-    return(new_list + my_list)
+    for i in range(len(my_list)):
+        del my_list[idx]
+        return(new_list + my_list)

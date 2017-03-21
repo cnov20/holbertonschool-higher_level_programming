f05d36d736327d50804be025cd663c9d0e4aeb05
cnov20 <128@holbertonschool.com>
2017-03-21 07:40:59 +0000

    Added check for empty tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    if tuple_a is None or tuple_b is None:
+        return
+
+    #if len(tuple_a) < 2 or len(tuple_b) < 2:
+
     sums = []
     new_a = tuple_a + (0, 0)
     new_b = tuple_b + (0, 0)

edd74d3376a7c49b81d70b70b67598bea5a4bc57
cnov20 <128@holbertonschool.com>
2017-03-21 19:00:21 +0000

    Added if statement to handle None condition

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def number_keys(my_dict):
     num_keys = 0
-    for k, v in enumerate(range(len(my_dict))):
-        num_keys += k
-    return (num_keys)
+    if my_dict:
+        for k, v in enumerate(range(len(my_dict))):
+            num_keys += k
+        return (num_keys)

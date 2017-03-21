dfc816f3289fc3807d381e9e8d3bab538f8af675
cnov20 <128@holbertonschool.com>
2017-03-21 11:33:01 +0000

    Fixed typo in for loop expression - missing iterator

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,5 +6,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_a = tuple_a + (0, 0)
     new_b = tuple_b + (0, 0)
     for i in range(2):
-        sums.append(new_a[] + new_b[i])
+        sums.append(new_a[i] + new_b[i])
     return(tuple(sums))

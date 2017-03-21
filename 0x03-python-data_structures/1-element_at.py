a6683d114738a85807538201338082700bd8c4a7
cnov20 <128@holbertonschool.com>
2017-03-21 00:52:06 +0000

    Refactored to handle negative idx given or index beyond length of list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for i in range(len(my_list)):
-        if idx < len(my_list):
-            return(my_list[idx])
-        else:
+        if idx < 0 or idx > len(my_list):
             return None
+        else:
+            return (my_list[idx])

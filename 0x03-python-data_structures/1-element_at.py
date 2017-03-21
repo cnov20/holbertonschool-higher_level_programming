a5d003afdad1c0c53f53d1b005e925502e6f0f4d
cnov20 <128@holbertonschool.com>
2017-03-21 00:47:07 +0000

    Added condition for if idx = length

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for i in range(len(my_list)):
-        if idx < len(my_list):
+        if idx <= len(my_list):
             return(my_list[idx])
         else:
             return None

25ba3fdb50ac7392c6f0c478a4e029697bf14204
cnov20 <128@holbertonschool.com>
2017-03-21 04:11:25 +0000

    Refactored, using index to iterated through matirx

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    index = 0
     for row in matrix:
-        for element in row:
-            print('{:d}'.format(element), end=' ')
+        for index in range(len(row)):
+            if index == (len(row) - 1):
+                print('{:d}'.format(row[index]), end='')
+            else:
+                print('{:d}'.format(row[index]), end=' ')
         print()

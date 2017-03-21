0df825d738f509e44eaaeb7f89175e083d72fb94
cnov20 <128@holbertonschool.com>
2017-03-21 19:10:59 +0000

    Completely re-factored and simplified using length and dict attribute

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def number_keys(my_dict):
-    num_keys = 0
-    if my_dict is not None:
-        for k, v in enumerate(range(len(my_dict))):
-            num_keys += k
-        return (num_keys)
-    else:
-        return (None)
+    num_keys = len(my_dict.keys())
+    return (num_keys)

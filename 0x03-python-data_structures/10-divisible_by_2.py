69fba1f462b3088694140b685807a77552d4aea3
cnov20 <128@holbertonschool.com>
2017-03-21 06:30:46 +0000

    Changed range of for loop

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
         return
 
     new_list = []
-    for i in range(len(my_list)):
+    for i in my_list:
         if i % 2 == 0:
             new_list.append(True)
         else:

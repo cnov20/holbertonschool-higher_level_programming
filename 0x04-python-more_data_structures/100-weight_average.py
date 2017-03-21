718c9be753694a1b80f013504541ec0833f0c2b5
cnov20 <128@holbertonschool.com>
2017-03-21 22:03:09 +0000

    Re-factored code using iterator and adding to score, weight - removed debugging print statements

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,9 +5,10 @@ def weight_average(my_list=[]):
     elif my_list is None:
         return (None)
     else:
-        for score in range(len(my_list)):
-            weight = my_list[score][1]
-            weight_avg = (score * weight / weight)
-#           print (weight)
-#           print (weight_avg)
+        weight = 0
+        score = 0
+        for i in range(len(my_list)):
+            score += (my_list[i][0] * my_list[i][1])
+            weight += my_list[i][1]
+        weight_avg = score / weight
         return (weight_avg)

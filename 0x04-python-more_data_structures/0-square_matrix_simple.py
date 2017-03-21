1968b6f4fb813413de36338461714c5b55daf974
cnov20 <128@holbertonschool.com>
2017-03-21 12:41:26 +0000

    Adjusted initialization and assigment to create two dimensional array

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squares = []
-    squares = list(map(lambda x: x**2, range(10)))
+    squares = [[]]
+    squares = [list(map(lambda x: x**2, range(10)))]
     return (squares)

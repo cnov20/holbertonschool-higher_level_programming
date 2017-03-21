2c9f84fb4380f3809fbed5de498783ade8466085
cnov20 <128@holbertonschool.com>
2017-03-21 20:11:29 +0000

    Fixed indent issue - pep8

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -8,6 +8,6 @@ def weight_average(my_list=[]):
         for score in range(len(my_list)):
             weight = my_list[score][1]
             weight_avg = (score * weight / weight)
-#            print (weight)
- #           print (weight_avg)
+#           print (weight)
+#           print (weight_avg)
         return (weight_avg)

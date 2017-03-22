55a46f8c244d65945a82284360c83b0e0491ec7f
cnov20 <128@holbertonschool.com>
2017-03-22 04:09:32 +0000

    Added 'as' to handle attribute of Error object - added str format for write()

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
-def safe_print_integer(value):
+def safe_print_integer_err(value):
         while True:
                 try:
                         print('{:d}'.format(value))
                         return (True)
-                except (ValueError, TypeError):
-                        sys.stderr.write('Exception:', file=sys.stderr)
+                except (ValueError, TypeError) as error:
+                        sys.stderr.write('Exception:' + str(error)+ '\n')
                         return (False)

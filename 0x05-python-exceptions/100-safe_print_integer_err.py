8a0aa275e70d7923f558044fa108b279934a56af
cnov20 <128@holbertonschool.com>
2017-03-22 04:11:15 +0000

    Fixed spacing issues - pep8

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
         while True:
                 try:
                         print('{:d}'.format(value))
                         return (True)
                 except (ValueError, TypeError) as error:
-                        sys.stderr.write('Exception:' + str(error)+ '\n')
+                        sys.stderr.write('Exception:' + str(error) + '\n')
                         return (False)

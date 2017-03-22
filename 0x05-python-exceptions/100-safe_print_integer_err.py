3ff02e4300aa13f538da00d031efca77ebb847e0
cnov20 <128@holbertonschool.com>
2017-03-22 04:13:33 +0000

    Removed while loop

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,6 @@ import sys
 
 
 def safe_print_integer_err(value):
-        while True:
                 try:
                         print('{:d}'.format(value))
                         return (True)

baa2ae163e2ec26cf9bd03aab814fa3eddd50814
cnov20 <128@holbertonschool.com>
2017-03-22 00:51:24 +0000

    Task 2 - safely print only integers in list - Initial

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+        i = 0
+        while True:
+                try:
+                        if i < x:
+                                print('{:d}'.format(my_list[i]), end='')
+                                i += 1
+                        else:
+                                print()
+                                return(i)
+                except TypeError:
+                        print()
+                        return(i)

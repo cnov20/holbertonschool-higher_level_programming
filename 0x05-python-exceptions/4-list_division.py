05fe0a93ed908d711dafdd1595df9818268ae82a
cnov20 <128@holbertonschool.com>
2017-03-22 02:25:49 +0000

    Task 4 - divide 2 lists, element by element - Initial

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            quotient = my_list_1[j] / my_list_2[j]
+            new_list = quotient
+        except ZeroDivisionError:
+            print('division by 0')
+        except ValueError:
+            print('wrong type')
+        except IndexError:
+            print('out of range')
+        finally:
+            
+            return(new_list)

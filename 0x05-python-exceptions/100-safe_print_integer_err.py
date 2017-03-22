86e2f16ee2510477b39be6aeb7c4d8e70f6278eb
cnov20 <128@holbertonschool.com>
2017-03-22 04:15:49 +0000

    Added space to error string and fixed indentation

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-                try:
-                        print('{:d}'.format(value))
-                        return (True)
-                except (ValueError, TypeError) as error:
-                        sys.stderr.write('Exception:' + str(error) + '\n')
-                        return (False)
+    try:
+        print('{:d}'.format(value))
+        return (True)
+    except (ValueError, TypeError) as error:
+        sys.stderr.write('Exception: ' + str(error) + '\n')
+        return (False)

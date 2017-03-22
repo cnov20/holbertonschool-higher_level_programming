d71fcc10560788d4e492443a42316a22f9b20359
cnov20 <128@holbertonschool.com>
2017-03-22 01:34:42 +0000

    Re-factored - for loop instead of while loop, removed if/else and added j as counter variable

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-        i = 0
-        while True:
+        j = 0
+        for i in range(x):
                 try:
-                        if i < x:
-                                print('{:d}'.format(my_list[i]), end='')
-                                i += 1
-                        else:
-                                print()
-                                return(i)
-                except TypeError:
-                        print()
-                        return(i)
+                        print('{:d}'.format(my_list[i]), end='')
+                        j += 1
+                except (TypeError, ValueError):
+                        continue
+        print()
+        return(j)

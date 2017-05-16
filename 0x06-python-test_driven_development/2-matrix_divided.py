d9b2fb33f6124d029e61740368b552cc902a8b9a
cnov20 <128@holbertonschool.com>
2017-05-16 08:12:22 +0000

    Added docstring comments to function file and test file, re-formatted test file comments

--- 0x06-python-test_driven_development/2-matrix_divided.py
+++ 0x06-python-test_driven_development/2-matrix_divided.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python3
+'''
+This is the 'matrix_divided' module.
+
+The matrix_divided module supplies a single function
+which takes two parameters - matrix_divided(matrix, div).
+
+For example:
+>>> matrix_divided(matrix, 3)
+[[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
+'''
+
+
+
+
 def matrix_divided(matrix, div):
+    '''
+    Function divides all elements of a matrix(list of lists)
+    by second parameter passed (div)
+
+    Parameters:
+    matrix -- a list of lists to be divided
+    div -- number to divide matrix by
+
+    Return: new matrix containing the quotients of aforementioned division
+    '''
 
     new_matrix = []
 
     for i in range(len(matrix)):
-
+        '''
         if type(div) is not int and type(div) is not float:
             raise TypeError ('div must be a number')
 
@@ -16,6 +40,10 @@ def matrix_divided(matrix, div):
             raise ZeroDivisionError('division by zero')
 
         else:
-            new_matrix += [list(map(lambda x: x / div, matrix[i]))]
+        '''
+        new_matrix += [list(map(lambda x: x / div, matrix[i]))]
 
+        '''    if len(new_matrix) != len(matrix):
+                raise TypeError('Each row of the matrix must have the same size')
+'''
     return (new_matrix)

94a510dbda8c2829ef8a71d7975b5be76fbd4e19
cnov20 <128@holbertonschool.com>
2017-05-16 11:34:32 +0000

    Completely re-factored - Uncommented exception raising condtionals - moved for loop to end of function - checked length or rows in matrix and element type, within for loop - added rounding to 2 decimal places functionality

--- 0x06-python-test_driven_development/2-matrix_divided.py
+++ 0x06-python-test_driven_development/2-matrix_divided.py
@@ -8,12 +8,10 @@ which takes two parameters - matrix_divided(matrix, div).
 For example:
 >>> matrix_divided(matrix, 3)
 [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
+
 '''
-
-
-
-
 def matrix_divided(matrix, div):
+
     '''
     Function divides all elements of a matrix(list of lists)
     by second parameter passed (div)
@@ -25,25 +23,22 @@ def matrix_divided(matrix, div):
     Return: new matrix containing the quotients of aforementioned division
     '''
 
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError ('div must be a number')
+
     new_matrix = []
 
     for i in range(len(matrix)):
-        '''
-        if type(div) is not int and type(div) is not float:
-            raise TypeError ('div must be a number')
 
-
-        if type(matrix) is not int and type(matrix) is not float:
+        if type(i) is not int and type(i) is not float:
             raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
 
-        if div == 0:
-            raise ZeroDivisionError('division by zero')
+        if len(matrix[i]) != len(matrix[0]):
+            raise TypeError('Each row of the matrix must have the same size')
 
-        else:
-        '''
-        new_matrix += [list(map(lambda x: x / div, matrix[i]))]
+        new_matrix += [list(map(lambda x: round(x / div, 2), matrix[i]))]
 
-        '''    if len(new_matrix) != len(matrix):
-                raise TypeError('Each row of the matrix must have the same size')
-'''
     return (new_matrix)

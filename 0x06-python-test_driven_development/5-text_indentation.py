e87761c1d0fedc3ed11c75fbd06603836bf7b596
cnov20 <128@holbertonschool.com>
2017-05-16 05:33:17 +0000

    Added join() function call, closer to desired output format

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -4,12 +4,12 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError('text must be a string')
 
-    words = text.split()
+#   words = text.split()
     i = 0
     letter = text[i]
     delimiter = set('.?:')
-    for letter in words:
-        if letter == '.':
+    for letter in text:
+        print('{:s} '.join(format(letter)), end='')
+
+        if letter in delimiter:
             print()
-            print()
-        print('{:s} '.format(letter), end='')

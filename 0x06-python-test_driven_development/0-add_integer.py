23593d72b1c37ba874d7c0e51fe0e1e093ab26a5
cnov20 <128@holbertonschool.com>
2017-05-16 00:32:25 +0000

    Added typecasting functionality and fixed indentation issue - pep8

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def add_integer(a, b):
+
     if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
 
     if type(b) is not int and type(b) is not float:
-       raise TypeError('b must be an integer')
+        raise TypeError('b must be an integer')
 
-    return (a + b)
+    return (int(a) + int(b))

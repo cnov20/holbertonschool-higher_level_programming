89885fc68f45390e095f3ad7c6e8fd68604328df
cnov20 <128@holbertonschool.com>
2017-05-16 08:32:03 +0000

    Added docstring comments to function file and test file, re-formatted test file

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
+'''
+This is the 'say_my_name' module.
+
+The say_my_name module supplies a single function
+which takes two parameters - say_my_name(first_name, last_name="").
+
+For example:
+>>> say_my_name("Walter", "White")
+My name is Walter White
+'''
+
 def say_my_name(first_name, last_name=""):
+    '''
+    Function prints the following:
+    'My name is' combined with first_name and last_name
+
+    Parameters:
+    first_name -- first name to be printed
+    last_name -- second (last) name to be printed
+
+    Return: No return value, only prints
+    '''
 
     if type(first_name) is not str:
         raise TypeError('first_name must be a string')

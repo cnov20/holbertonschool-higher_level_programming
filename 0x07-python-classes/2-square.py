85d32096a624cf9beaba5cdac4af43d12ae3464a
cnov20 <128@holbertonschool.com>
2017-05-18 03:37:50 +0000

    Added type check for int to conditionals - basic functionality is working

--- 0x07-python-classes/2-square.py
+++ 0x07-python-classes/2-square.py
@@ -5,9 +5,9 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
-        if type(size) and size >= 0:
+        if type(size) is int and size >= 0:
             self.__size = size
-        elif type(size) and size < 0:
+        elif type(size) is int and size < 0:
             raise ValueError('size must be >= 0')
         else:
             raise TypeError('size must be an integer')

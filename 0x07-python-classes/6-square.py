7b50255ea3201683d0dfb6f74dc92acf973ad6cf
cnov20 <128@holbertonschool.com>
2017-05-18 10:33:39 +0000

    Added error check for tuple that does not have two proper values

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -26,7 +26,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple and value < 0:
+        if type(value) is not tuple or len(value) !=  2:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 

78c3e960d8a1caf1001374f3a9192ac571794230
cnov20 <128@holbertonschool.com>
2017-05-30 18:33:30 +0000

    Added Task 1 - write a MyList class that inherits from list built-in object type and prints in ascending order

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+
+''' Module that creates a MyList class which inherits directly from
+    built-in list() object '''
+
+class MyList(list):
+
+    ''' Prints a sorted version of a list instance '''
+
+    def print_sorted(self):
+        sorted_list = MyList()
+        for i in self:
+            sorted_list.append(i)
+        print(sorted(sorted_list))

e38a41c6d10fe5e86ed1990d99644f5904e59e63
cnov20 <128@holbertonschool.com>
2017-05-31 21:58:31 +0000

    Added Task 4 - append to text file

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -3,10 +3,10 @@
 ''' Module that writes string to a text file and returns num of chars'''
 
 
-def write_file(filename='', text=''):
+def append_write(filename='', text=''):
 
     ''' Writes string to text file and returns num of chars written'''
 
-    with open('my_first_file.txt', 'w', encoding='utf-8') as a_file:
+    with open('file_append.txt', 'a', encoding='utf-8') as a_file:
         a_file = a_file.write("Holberton School is so cool!\n")
         return (a_file)

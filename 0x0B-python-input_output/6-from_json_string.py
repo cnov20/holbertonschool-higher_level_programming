8f56390a45e1d8ebcb1581b8b620c53ea54e3b05
cnov20 <128@holbertonschool.com>
2017-05-31 22:15:40 +0000

    Added Task 6 - deserialize a JSON str to Python object

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import json
+''' Module that deserializes a str to JSON object '''
+
+def from_json_string(my_str):
+
+    ''' Converts str to JSON object and returns '''
+
+    return(json.loads(my_str))

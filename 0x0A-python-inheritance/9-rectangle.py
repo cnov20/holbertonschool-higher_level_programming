24fdfb03722d57694aa74386250ae684e66b1cdf
cnov20 <128@holbertonschool.com>
2017-05-31 00:25:44 +0000

    pep8 indentation fixed

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -40,4 +40,4 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         ''' Returns formatted string with subclass attribute values '''
         return ('[{}] {}/{}'.format(type(self).__name__, self.__width,
-                                  self.__height))
+                                    self.__height))

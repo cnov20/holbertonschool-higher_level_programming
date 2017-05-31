5c85ed9fd1d9aac2109d6fb8be26a5a9a0d84e9a
cnov20 <128@holbertonschool.com>
2017-05-31 00:44:13 +0000

    Refactored code - changed logic to reflect False for single condition and True for other two being tested

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,6 +9,6 @@ def inherits_from(obj, a_class):
     ''' Returns true if an object is an instance of specified class
         that inherited directly or indirectly - otherwise, False'''
 
-    if issubclass(a_class, type(obj)) or type(obj) is a_class:
-        return True
-    return False
+    if issubclass(a_class, type(obj)):
+        return False
+    return True

280634e84797eb6fdc4bddeec314b380eb547826
cnov20 <128@holbertonschool.com>
2017-05-31 21:20:21 +0000

    Added Task 0 - open and read text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+''' Module that reads a text file and prints to std out '''
+
+def read_file(filename=""):
+
+    ''' Read file and print to std out '''
+
+with open('my_file_0.txt', encoding='utf-8') as a_file:
+    a_file = a_file.read()
+    print (a_file)

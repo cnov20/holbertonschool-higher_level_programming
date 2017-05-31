d0ba50145290625f6581ad181c161985721f4035
cnov20 <128@holbertonschool.com>
2017-05-31 22:01:55 +0000

    Corrected docstrings

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-''' Module that writes string to a text file and returns num of chars'''
+''' Module that appends string to text file'''
 
 
 def append_write(filename='', text=''):
 
-    ''' Writes string to text file and returns num of chars written'''
+    ''' Appends string to text file and returns num of chars added'''
 
     with open('file_append.txt', 'a', encoding='utf-8') as a_file:
         a_file = a_file.write("Holberton School is so cool!\n")

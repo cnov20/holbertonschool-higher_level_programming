6b49c13b54c086d5a966c5391ca832f097410e01
cnov20 <128@holbertonschool.com>
2017-05-31 21:20:55 +0000

    Added Task 1 - Count and return number of lines in a text file

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+''' Module that returns the number of lines of a text file '''
+
+def number_of_lines(filename=""):
+
+    ''' Read file and return number of lines '''
+
+    with open('my_file_0.txt', encoding='utf-8') as a_file:
+        a_file = a_file.readlines()
+    return(len(a_file))

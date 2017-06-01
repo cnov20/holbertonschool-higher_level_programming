cf6965dc9eb43f63de7c3e2763abc3832a22bc26
cnov20 <128@holbertonschool.com>
2017-06-01 05:54:20 +0000

    Added Task 12 - retrieve dict representation of Student instance - only certain attributes

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+''' Module that creates a Student class and returns JSON dict rep '''
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        data = {}
+        if attrs is None:
+            data = self.__dict__
+            return data
+        for i in range(len(attrs)):
+            if hasattr(self, attrs[i]):
+                data[attrs[i]] = getattr(self, attrs[i])
+        return (data)

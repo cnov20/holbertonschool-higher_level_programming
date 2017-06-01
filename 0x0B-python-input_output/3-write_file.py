b0cf2d132d338eae19e013078dd722f9b76faa6c
cnov20 <128@holbertonschool.com>
2017-06-01 04:26:02 +0000

    Replaced hard coded text with text parameter

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -8,5 +8,5 @@ def write_file(filename='', text=''):
     ''' Writes string to text file and returns num of chars written'''
 
     with open(filename, 'w', encoding='utf-8') as a_file:
-        a_file = a_file.write("Holberton School is so cool!\n")
+        a_file = a_file.write(text)
         return (a_file)

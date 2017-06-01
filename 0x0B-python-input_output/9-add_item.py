749c8713f7b44b834f9c3e7e9c7bb9b7c3f8ca30
cnov20 <128@holbertonschool.com>
2017-06-01 00:28:20 +0000

    Added Task 9 - Script that adds cmd line arguments to a python list, then saves to file

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from sys import argv
+import json
+import os.path
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+''' Module that adds arguments from cmd line to Python list and saves to file'''
+
+filename = 'add_item.json'
+my_list = []
+if os.path.exists(filename):
+    load_from_json_file(filename)
+else:
+    save_to_json_file(my_list, filename)
+
+for i in argv[1:]:
+    my_list.append(i)
+
+save_to_json_file(my_list, filename)

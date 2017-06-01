b661ac0496767b49f44ef89e7f6bc151b1421ec4
cnov20 <128@holbertonschool.com>
2017-06-01 04:27:38 +0000

    Replaced hard coded values - file / text, with corresponding parameters

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -7,6 +7,6 @@ def append_write(filename='', text=''):
 
     ''' Appends string to text file and returns num of chars added'''
 
-    with open('file_append.txt', 'a', encoding='utf-8') as a_file:
-        a_file = a_file.write("Holberton School is so cool!\n")
+    with open(filename, 'a', encoding='utf-8') as a_file:
+        a_file = a_file.write(text)
         return (a_file)

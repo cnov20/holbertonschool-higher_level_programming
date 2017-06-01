d34e304b46af3ea619aeace54521c9185cb33ddd
cnov20 <128@holbertonschool.com>
2017-06-01 04:21:37 +0000

    Indented return statement and used filename param, rather than hardcoded file name

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -7,6 +7,6 @@ def number_of_lines(filename=""):
 
     ''' Read file and return number of lines '''
 
-    with open('my_file_0.txt', encoding='utf-8') as a_file:
+    with open(filename, encoding='utf-8') as a_file:
         a_file = a_file.readlines()
-    return(len(a_file))
+        return(len(a_file))

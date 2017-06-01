944572e69b9167a8c8cbcd67cd0132646db6426f
cnov20 <128@holbertonschool.com>
2017-06-01 04:24:29 +0000

    Replace hard coded file name with file name parameter

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -7,6 +7,6 @@ def write_file(filename='', text=''):
 
     ''' Writes string to text file and returns num of chars written'''
 
-    with open('my_first_file.txt', 'w', encoding='utf-8') as a_file:
+    with open(filename, 'w', encoding='utf-8') as a_file:
         a_file = a_file.write("Holberton School is so cool!\n")
         return (a_file)

0468c395bb836658fca4caf646afe376eae6c204
cnov20 <128@holbertonschool.com>
2017-06-01 04:23:03 +0000

    Replace hard coded file name with filename parameter

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -8,7 +8,7 @@ def read_lines(filename="", nb_lines=0):
     ''' Read (n) lines of file and print to stdout'''
 
     total_lines = 0
-    with open('my_file_0.txt', encoding='utf-8') as a_file:
+    with open(filename, encoding='utf-8') as a_file:
         for a_line in a_file:
             total_lines += 1
             if nb_lines <= 0 or nb_lines >= total_lines:

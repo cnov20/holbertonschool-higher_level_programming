dadf244ffe62efeb538e45d8b8be21ad4d841c7d
cnov20 <128@holbertonschool.com>
2017-07-11 23:03:24 +0000

    Initial - convert db/table/field to UTF 8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- Script converts db, table and field to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

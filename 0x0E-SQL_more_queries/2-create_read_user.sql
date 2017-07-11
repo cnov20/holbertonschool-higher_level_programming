b1686dc209fdb38e88fd90e6aea606d19b2e02ee
cnov20 <128@holbertonschool.com>
2017-07-11 19:18:42 +0000

    Updated script description

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Creates user with all privileges on MySQL server
+-- Creates database and user, if they do not exist, with SELECT privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';

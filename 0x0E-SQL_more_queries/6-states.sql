82498b3d37983649049c6a1232801850d747dd6e
cnov20 <128@holbertonschool.com>
2017-07-11 21:44:33 +0000

    Initial - create table and database if they do not exist - NOT NULL / PK / AUTO INCREMENT

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Creates a table if doesn't exist and field unique, default value, pk
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256) NOT NULL);

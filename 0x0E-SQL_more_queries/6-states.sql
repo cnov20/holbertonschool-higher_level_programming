515af29b5f098f736c24566ad8d401bd48dc93d8
cnov20 <128@holbertonschool.com>
2017-07-11 22:32:49 +0000

    Adjusted primary key command placement

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,7 @@
 -- Creates a db and table if doesn't exist and field unique, default value, pk
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states.htbn_0d_usa (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+id INT NOT NULL AUTO_INCREMENT UNIQUE,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id)
+);

9ce7e1a7e603fe5b0f3b95ffb793f178ee16f4b6
cnov20 <128@holbertonschool.com>
2017-07-12 15:42:16 +0000

    Re-ordered constraints for readibility:

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Creates a db and table if doesn't exist and field unique, default value, pk
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(256)
 );

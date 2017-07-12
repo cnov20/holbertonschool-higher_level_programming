2c94357cb15179ea1ccc00a24f4fdaa35c9f347f
cnov20 <128@holbertonschool.com>
2017-07-12 19:43:33 +0000

    Proper relationship, need to take care of NULL values now

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,5 +2,8 @@
 -- via join method
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-LEFT JOIN tv_genres ON tv_shows.id = tv_genres.id
+LEFT JOIN tv_show_genres
+     ON tv_shows.id = tv_show_genres.show_id
+     JOIN tv_genres
+     ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name  ASC;

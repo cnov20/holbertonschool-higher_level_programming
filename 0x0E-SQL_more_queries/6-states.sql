0412c67c21c154ea04a5f6f44334229e4e30cab7
cnov20 <128@holbertonschool.com>
2017-07-12 15:11:05 +0000

    Refactored - put PRIMARY KEY command in id contrsraint line

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- Creates a db and table if doesn't exist and field unique, default value, pk
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-id INT NOT NULL AUTO_INCREMENT UNIQUE,
+id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 name VARCHAR(256) NOT NULL,
-PRIMARY KEY(id)
 );

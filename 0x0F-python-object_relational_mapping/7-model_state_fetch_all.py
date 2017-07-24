969579daec62965c035af861c915e0abe492e776
cnov20 <128@holbertonschool.com>
2017-07-24 14:29:25 +0000

    Indented within main condition, so code does not execute when imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,14 +10,14 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ = '__main__':
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                        .format(argv[1], argv[2], argv[3]))
-Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
-for state in session.query(State).order_by(State.id).all():
-    # HERE: no SQL query, only objects!
-    print("{}: {}".format(state.id, state.name))
-session.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        # HERE: no SQL query, only objects!
+        print("{}: {}".format(state.id, state.name))
+    session.close()

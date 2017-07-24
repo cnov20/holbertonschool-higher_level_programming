860a98d376bc1edfdd0e3f871c5f04cedc81e3fe
cnov20 <128@holbertonschool.com>
2017-07-24 15:14:05 +0000

    Task 8 - print first object of a table from database

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+''' Module that lists the first object in a given table, using SQLAlchemy,
+    this is linked to a MySQL table via SQLAlchemy '''
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_state = session.query(State).first()
+    # HERE: no SQL query, only objects!
+    print("{}: {}".format(first_state.id, first_state.name))
+    session.close()

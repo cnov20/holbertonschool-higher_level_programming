5bbb1b13f6398b708c3317180df48a5dc9ed8b91
cnov20 <128@holbertonschool.com>
2017-07-24 08:41:53 +0000

    Moved close function calls outside of foor loop and added double underscores to main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,13 +5,13 @@
 from sys import argv
 import MySQLdb
 
-if __name__ == 'main':
+if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            password=argv[2], db=argv[3])
     cursor = conn.cursor()
-    cursor.execute("SELECT states.id, states.name FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cursor.fetchall()
     for row in query_rows:
         print(row)
-        cursor.close()
-        conn.close()
+    cursor.close()
+    conn.close()

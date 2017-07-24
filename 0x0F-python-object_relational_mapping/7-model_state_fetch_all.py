d32ead0e5bd4dda634348d73e53368be6835ce59
cnov20 <128@holbertonschool.com>
2017-07-24 14:25:06 +0000

    Added if condition to only execute if main function

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
+if __name__ = '__main__':
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                        .format(argv[1], argv[2], argv[3]))
 Base.metadata.create_all(engine)

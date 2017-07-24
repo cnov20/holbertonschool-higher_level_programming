8cf0a2ed63e5421c3120d2aa739461d925cfc2eb
cnov20 <128@holbertonschool.com>
2017-07-24 08:51:00 +0000

    Added Task 1 - Initial - Select values with a certain attribute, from a specific column

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+''' Module that lists all fields in a given column with a specific attribute'''
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           password=argv[2], db=argv[3])
+    cursor = conn.cursor()
+    cursor.execute("SELECT states.id, states.name FROM states WHERE states.name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    conn.close()

1a5ae9ac5ae3b7a5215e556899b804a3e5ae5b91
cnov20 <128@holbertonschool.com>
2017-07-24 08:55:37 +0000

    Added backslash escape character to continue script on new line

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            password=argv[2], db=argv[3])
     cursor = conn.cursor()
-    cursor.execute("SELECT states.id, states.name FROM states WHERE states.name
+    cursor.execute("SELECT states.id, states.name FROM states WHERE states.name\
                    LIKE 'N%' ORDER BY id ASC")
     query_rows = cursor.fetchall()
     for row in query_rows:

dbf5030c699ace77d9907cacd31b1727771e92d7
cnov20 <128@holbertonschool.com>
2017-07-24 08:26:55 +0000

    Moved if statement to beginning of script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,15 +5,13 @@
 from sys import argv
 import MySQLdb
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                       password=argv[2], db=argv[3])
-cursor = conn.cursor()
-cursor.execute("SELECT states.id, states.name FROM states ORDER BY id ASC")
-query_rows = cursor.fetchall()
-for row in query_rows:
-    print(row)
-cursor.close()
-conn.close()
-
 if __name__ == 'main':
-    main()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           password=argv[2], db=argv[3])
+    cursor = conn.cursor()
+    cursor.execute("SELECT states.id, states.name FROM states ORDER BY id ASC")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+        cursor.close()
+        conn.close()

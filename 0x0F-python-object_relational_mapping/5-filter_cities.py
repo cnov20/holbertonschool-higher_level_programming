526be9bc47a671676c014a2865a57d547fa992e2
cnov20 <128@holbertonschool.com>
2017-07-24 11:37:09 +0000

    Added index and if statement - with enumerate, to get proper print formatting

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,8 +14,11 @@ if __name__ == '__main__':
     INNER JOIN states ON cities.state_id = states.id WHERE states.name='{}'\
     ORDER BY cities.id ASC".format(argv[4]))
     query_rows = cursor.fetchall()
-    for row in query_rows:
-        print("{}, ".format(''.join(row)), end='')
+    index = 0
+    for index, row in enumerate(query_rows):
+        if index > 0:
+            print(', ', end='')
+        print('{}'.join(row), end='')
     print()
     cursor.close()
     conn.close()

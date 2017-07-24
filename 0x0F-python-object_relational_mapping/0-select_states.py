c4e5cafb8c873a5fa6a5ef2b0637fd872d158e2c
cnov20 <128@holbertonschool.com>
2017-07-24 08:16:53 +0000

    Added Task 0 - list all fields of column from MySQL database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+''' Module that lists all fields in a given column '''
+
+from sys import argv
+import MySQLdb
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                       password=argv[2], db=argv[3])
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cursor.fetchall()
+for row in query_rows:
+    print(row)
+cursor.close()
+conn.close()

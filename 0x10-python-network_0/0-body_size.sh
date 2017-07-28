73c5c0cd3a922405ca0cd85eaec3baca6d33b988
cnov20 <128@holbertonschool.com>
2017-07-28 16:40:47 +0000

    Changed delimiter to space instead of colon

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a 'curl' cmd  request to a given URL and displays size of response body
-curl -sI 0.0.0.0:5000 | grep Content-Length | cut -d ':' -f2
+curl -sI 0.0.0.0:5000 | grep Content-Length | cut -d ' ' -f2

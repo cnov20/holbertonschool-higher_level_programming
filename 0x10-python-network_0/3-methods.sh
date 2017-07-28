fc3cc8e0317e0f0c356d61ca5de52c895bb07886
cnov20 <128@holbertonschool.com>
2017-07-28 17:48:37 +0000

    Added translate (tr) command to delete excess new line

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Uses curl - dispays all HTTP method a server will accept - given URL
-curl -si --request OPTIONS "$1" | grep Allow | cut -d ' ' -f2-4
+curl -si --request OPTIONS "$1" | grep Allow | cut -d ' ' -f2-4 | tr -d '\n'

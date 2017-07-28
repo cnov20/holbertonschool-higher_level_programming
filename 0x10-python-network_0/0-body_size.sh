c2ff80c559e8caff135223c5dbf094955062a8a1
cnov20 <128@holbertonschool.com>
2017-07-28 16:51:24 +0000

    Trying revamped version to take input from cmd line, of initial solution

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a 'curl' cmd  request to a given URL and displays size of response body
-curl -sI "$1" | grep Content-Length | cut -d ' ' -f2
+curl -s -o /dev/null "$1" --write-out '%{size_download}\n'

f183c85c50f5efbb956c8ae0face0f89b5eb04ee
cnov20 <128@holbertonschool.com>
2017-07-28 16:05:33 +0000

    Added flags and /dev/null pseudo file to suppress body output

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a 'curl' cmd  request to a given URL and displays size of response body
-curl 0.0.0.0:5000 --write-out '%{size_download}'
+curl -s -o /dev/null 0.0.0.0:5000 --write-out '%{size_download}\n'

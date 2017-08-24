5feec08e0722f9928796322151695d97108fc8e5
cnov20 <128@holbertonschool.com>
2017-08-24 06:21:02 +0000

    Added header flag to add to origin, to indicate that origin is Holberton School

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Uses curl - GET request - causes specific response body from server
-curl -s --request GET 0.0.0.0:5000/catch_me --data "user_id=98"
+curl -sL --header 'Origin:HolbertonSchool' --request PUT 0.0.0.0:5000/catch_me --data "user_id=98"

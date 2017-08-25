9b1bdc79881b18a328a2cde2ef4f7e73aaa05eae
cnov20 <128@holbertonschool.com>
2017-08-25 18:09:30 +0000

    Added Task 1 - script takes in URL - sends request to URL and displays value of specific header variable - Currently hardcoded, need to take command line arg - Initial

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+''' This script takes in a URL from cmd line - sends reuest to URL
+    and displays value of specific variabled found in header of response '''
+
+if __name__ == "__main__":
+
+    import urllib.request
+    from sys import argv
+
+    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        holberton = dict(response.info())
+        for k, v in holberton.items():
+            if k == 'X-Request-Id':
+                print(v)

723b95b7133f4ffed035d2636716299ec7a658d3
cnov20 <128@holbertonschool.com>
2017-08-25 20:11:42 +0000

    Added Task 5 - Takes URL from cmd line - sends GET request to given URL - displays header variable from response - using requests package - Initial

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+''' This script takes in a URL from cmd line - sends reuest to URL
+    and displays value of specific variabled found in header of response
+    using requests package '''
+
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    req = requests.get(url)
+    headers = req.headers
+    x_request_id = headers['X-Request-Id']
+    print(x_request_id)

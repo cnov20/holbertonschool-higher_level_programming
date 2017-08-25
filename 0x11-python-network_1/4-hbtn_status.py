e65967bd92117dfea11957ea1979deb00d0264de
cnov20 <128@holbertonschool.com>
2017-08-25 19:41:40 +0000

    Added Task 4 - GET request to given URL - now using 'requests' package rather than 'urllib' - much simpler - Initial

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+''' This script fetches a URL - displays body of response
+    in specific format '''
+
+if __name__ == "__main__":
+
+    import requests
+    url = 'https://intranet.hbtn.io/status'
+    req = requests.get(url)
+    content = req.content
+    print('Body response:')
+    print('\t - type: {}'.format(type(content)))
+    print('\t - content: {}'.format(content.decode('utf-8')))

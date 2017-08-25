9f1ed9b5daacb85f15ce9db8c620a558614c4391
cnov20 <128@holbertonschool.com>
2017-08-25 18:10:52 +0000

    Added command line arg input feature

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     import urllib.request
     from sys import argv
 
-    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    req = urllib.request.Request(argv[1])
     with urllib.request.urlopen(req) as response:
         holberton = dict(response.info())
         for k, v in holberton.items():

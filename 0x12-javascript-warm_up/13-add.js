917bf6c6fd331315774be956f9059256a1685671
cnov20 <128@holbertonschool.com>
2017-09-13 22:53:01 +0000

    Removed unecessary variables that were processing command line user input, this fixed semistandard styling / specificatins as well

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 
-let args = process.argv;
-let numArgs = args.slice(2);
-let a = parseInt(numArgs[0]);
-let b = parseInt(numArgs[1]);
-
 exports.add = function (a, b) {
-  if (numArgs <= 0) {
-    return a + b;
+  if (!a || !b) {
+    return false;
   } else {
     return a + b;
   }

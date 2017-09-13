140b84f5832b7a89936d532d7e8780b43045e974
cnov20 <128@holbertonschool.com>
2017-09-13 21:39:45 +0000

    Changed name of array to check if reserved word is an issue

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 array.forEach(function (element) {
   console.log(element);

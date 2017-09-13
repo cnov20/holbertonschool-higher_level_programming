40b8e3fa669994e6fc03bd34ab471924c6f1aa83
cnov20 <128@holbertonschool.com>
2017-09-13 21:47:39 +0000

    Changed let variabled declaration to const

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 // Languages
-let languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-// languages.forEach(function (element) {
-//  console.log(element);
-// });
+languages.forEach(function (element) {
+  console.log(element);
+});
 
-for(let i = 0; i < languages.length; i++) {
-    console.log(languages[i]);
-}
+// for (let i = 0; i < languages.length; i++) {
+//  console.log(languages[i]);
+// }

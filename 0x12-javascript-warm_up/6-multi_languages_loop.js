2cc64fbc3f6aecaf082b1401164b5de0e658bb45
cnov20 <128@holbertonschool.com>
2017-09-13 21:43:17 +0000

    Switched to 'for' loop rather than 'forEach' to test if Holberton checker does not accept the latter for some reason, seems to be the case

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,10 @@
 // Languages
 let languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-languages.forEach(function (element) {
-  console.log(element);
-});
+// languages.forEach(function (element) {
+//  console.log(element);
+// });
+
+for(let i = 0; i < languages.length; i++) {
+    console.log(languages[i]);
+}

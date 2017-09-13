445b0f17126cd51e93c11c2db182e5b6a69c6fd7
cnov20 <128@holbertonschool.com>
2017-09-13 21:29:01 +0000

    Corrected output message - had an extra 'is' in message

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,9 +4,9 @@ let args = process.argv;
 let numArgs = args.slice(2);
 let words = numArgs.toString().split(',');
 let nums = parseInt(words);
-let myNum = 'My number is: ';
+let myNum = 'My number: ';
 
-if (numArgs <= 0 || !nums) {
+if (numArgs.length === 0 || !nums) {
   console.log('Not a number');
 } else {
   console.log(myNum + nums);

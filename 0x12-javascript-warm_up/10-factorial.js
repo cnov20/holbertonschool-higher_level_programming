90cb700aecfb86e61fdf0ca2b785bfcf8e98dd0d
cnov20 <128@holbertonschool.com>
2017-09-13 22:22:13 +0000

    Corrected for semi-standard style specs

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,8 +4,8 @@ let args = process.argv;
 let numArgs = args.slice(2);
 let num = parseInt(numArgs[0]);
 
-function factorial(num) {
-  if (num == 0) {
+function factorial (num) {
+  if (num === 0) {
     return (1);
   } else {
     return (num * factorial(num - 1));

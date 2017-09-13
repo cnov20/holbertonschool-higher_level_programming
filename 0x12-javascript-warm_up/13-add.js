78e0e6c23a366581e3bdbdbe6164b6a576562c5f
cnov20 <128@holbertonschool.com>
2017-09-13 22:46:12 +0000

    Changed console.log to return statement

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -2,14 +2,13 @@
 
 let args = process.argv;
 let numArgs = args.slice(2);
-let  a = parseInt(numArgs[0]);
-let  b = parseInt(numArgs[1]);
+let a = parseInt(numArgs[0]);
+let b = parseInt(numArgs[1]);
 
 exports.add = function (a, b) {
-
   if (numArgs <= 0) {
-    console.log(a + b);
+    return a + b;
   } else {
-    console.log(a + b);
+    return a + b;
   }
 };

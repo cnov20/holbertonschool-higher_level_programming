339d8e815929c04212d3f50bb1abcd11fcd0d5d2
cnov20 <128@holbertonschool.com>
2017-09-13 22:21:11 +0000

    Added Task 10 - Factorial Recursion - every case working except base

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let args = process.argv;
+let numArgs = args.slice(2);
+let num = parseInt(numArgs[0]);
+
+function factorial(num) {
+  if (num == 0) {
+    return (1);
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
+
+console.log(factorial(num));

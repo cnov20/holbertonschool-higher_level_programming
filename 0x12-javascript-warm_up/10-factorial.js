e565e1740d120ebf70a679dc0db3ada24a32da9c
cnov20 <128@holbertonschool.com>
2017-09-13 22:23:50 +0000

    Added check for negative numbers

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,7 +5,9 @@ let numArgs = args.slice(2);
 let num = parseInt(numArgs[0]);
 
 function factorial (num) {
-  if (num === 0) {
+  if (num <0) {
+    return (-1);
+} else if (num === 0) {
     return (1);
   } else {
     return (num * factorial(num - 1));

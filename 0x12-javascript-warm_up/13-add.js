2d44486f4526fe3f666092637db5872f83320bfb
cnov20 <128@holbertonschool.com>
2017-09-13 22:44:03 +0000

    Moved variables back to original places outside of function

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -2,10 +2,10 @@
 
 let args = process.argv;
 let numArgs = args.slice(2);
+let  a = parseInt(numArgs[0]);
+let  b = parseInt(numArgs[1]);
 
 exports.add = function (a, b) {
-  a = parseInt(numArgs[0]);
-  b = parseInt(numArgs[1]);
 
   if (numArgs <= 0) {
     console.log(a + b);

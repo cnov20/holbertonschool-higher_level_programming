3993181dfb8890d937d51fc177e9066ea0292d93
cnov20 <128@holbertonschool.com>
2017-09-14 01:30:15 +0000

    Corrected if condition

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,7 +7,7 @@ let num = parseInt(numArgs);
 // for i < numArgs.length
 //    numArgs[i] = parseInt(numArgs[i])
 function sortArgs (num) {
-  if (!numArgs || numArgs.length === 1) {
+  if (!num || numArgs.length === 1) {
     return (0);
   } else {
     numArgs.sort(function (a, b) {

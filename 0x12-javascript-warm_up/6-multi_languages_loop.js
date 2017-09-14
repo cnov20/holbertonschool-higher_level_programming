a039127267b8cd71e5d0c4af53bcf2c1d8953954
cnov20 <128@holbertonschool.com>
2017-09-14 00:17:14 +0000

    Removed commented out forEach loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,10 +2,6 @@
 
 const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-languages.forEach(function (element) {
-  console.log(element);
-});
-
-// for (let i = 0; i < languages.length; i++) {
-//  console.log(languages[i]);
-// }
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}

0136a262f162d84004347a18e2ffe9efc29b87f2
cnov20 <128@holbertonschool.com>
2017-09-15 04:52:28 +0000

    Changed var to let

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,10 +2,11 @@
 // Function that prints the num of arguments already printed)
 // as well as the new argument value
 
-var current = 0;
+let current = 0;
 module.exports.logMe = function (item) {
+
   if (item) {
     console.log(current + ': ' + item);
   }
-  current += 1;
+    current += 1;
 };

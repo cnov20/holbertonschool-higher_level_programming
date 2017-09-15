3c6a7ffd59e05a1a93062ad672874cda12e33dd9
cnov20 <128@holbertonschool.com>
2017-09-15 03:30:59 +0000

    Fixed for semistandard specs

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -10,7 +10,7 @@ Square.prototype.charPrint = function (c) {
 
   for (let i = 0; i < this.height; i++) {
     for (let j = 0; j < this.width; j++) {
-	    process.stdout.write(c);
+      process.stdout.write(c);
     }
     console.log();
   }

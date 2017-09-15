0e393d3d730c2ea4402b7d50d8f377129c24d80a
cnov20 <128@holbertonschool.com>
2017-09-15 04:18:19 +0000

    Moved export of Square constructor function to bottom of file

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,7 +2,11 @@
 // Imports Rectangle Object / Class
 // Uses this to define Square class which inherits from Rectangle
 // Does this via constructor call to parent class
+
 const Rectangle = require('./4-rectangle').Rectangle;
-module.exports.Square = function Square (size) {
+
+function Square (size) {
   Rectangle.call(this, size, size);
-};
+}
+
+module.exports.Square = Square;

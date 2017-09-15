c6bea5650536bf90b88f26c85ee6d525be4fee0d
cnov20 <128@holbertonschool.com>
2017-09-15 03:33:57 +0000

    Corrected for semistandard specs

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Function that returns the number of given element in array(list)
 
-module.exports.nbOccurences = function (list, search_element) {
+module.exports.nbOccurences = function (list, searchElement) {
   let occurences = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] === search_element) {
-	    occurences += 1;
+    if (list[i] === searchElement) {
+      occurences += 1;
     }
   }
   return (occurences);

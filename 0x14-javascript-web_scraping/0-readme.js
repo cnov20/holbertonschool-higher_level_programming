4deb3f6c18620f7d9df1997552cc20cdccf34a49
cnov20 <128@holbertonschool.com>
2017-09-26 22:55:07 +0000

    Task 0 - Initial add - script that reads contents of a file passed as command line argument

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Script that reads and prints the content of a file
+// passed as argument on command line
+
+let fs = require('fs');
+let file = process.argv[2];
+fs.readFile(file, 'utf8', function (error, text) {
+  if (error) {
+    throw error;
+  }
+  process.stdout.write(text);
+});

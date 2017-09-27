131af2db238e4c17d83660d8875c124bc6c483f9
cnov20 <128@holbertonschool.com>
2017-09-27 03:56:23 +0000

    Wrapped parsing, looping and counting in else statment - to only happen if no error occurs

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,18 +10,18 @@ const characterIdUrl = 'https://swapi.co/api/people/18/';
 request(url, function (err, res, body) {
   if (err) {
     console.log(err);
-  }
-
-  let json = JSON.parse(body);
-  let films = json.results;
-  let count = 0;
-  for (let i = 0; i < films.length; i++) {
-    let characters = films[i].characters;
-    for (let j = 0; j < characters.length; j++) {
-      if (characters[j] === characterIdUrl) {
-        count += 1;
+  } else {
+    let json = JSON.parse(body);
+    let films = json.results;
+    let count = 0;
+    for (let i = 0; i < films.length; i++) {
+      let characters = films[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === characterIdUrl) {
+          count += 1;
+        }
       }
     }
+    console.log(count);
   }
-  console.log(count);
 });

eac5640931e43924f6e72679f01a516bdd4539a4
cnov20 <128@holbertonschool.com>
2017-10-01 16:32:15 +0000

    Task 13 - Advanced - Concatenate two files into single file, using filestreams and pipes

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// This script takes in 2 files from the command line as arguments
+// And it concatenates them
+
+const fs = require('fs');
+
+const fileOne = process.argv[2];
+const fileTwo = process.argv[3];
+const fileDestination = process.argv[4];
+
+// open destination file for appending
+let w = fs.createWriteStream(fileDestination);
+// open source file(s) for reading
+let r = fs.createReadStream(fileOne);
+let r2 = fs.createReadStream(fileTwo);
+
+w.on('close', function () {
+  console.log('done writing');
+});
+
+r.pipe(w);
+r2.pipe(w);

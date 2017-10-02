7fda528815e9dd3795d89496fef2391144765195
cnov20 <128@holbertonschool.com>
2017-10-02 12:44:37 +0000

    Corrected to handle case where fileA is empty but not fileB

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -9,15 +9,21 @@ const fileOne = process.argv[2];
 const fileTwo = process.argv[3];
 const fileDestination = process.argv[4];
 
+//console.log(process.argv.length);
+
+if (process.argv.length !== 5) {
+    console.log("error");
+}
+
 // open destination file for appending
 let w = fs.createWriteStream(fileDestination);
 // open source file(s) for reading
 let r = fs.createReadStream(fileOne);
 let r2 = fs.createReadStream(fileTwo);
 
-// w.on('close', function () {
+//w.on('close', function () {
 //  console.log('done writing');
 // });
 
-r.pipe(w);
-r2.pipe(w);
+r.pipe(w, { end: false });
+r2.pipe(w, { end: false });
